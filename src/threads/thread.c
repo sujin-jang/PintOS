01b8f3b967f03919d28d5eb58f69eab33516b3d8
Haney Kang <haney1357@kaist.ac.kr>
2017-09-16 22:30:19 +0900

    git test

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -575,3 +575,5 @@ allocate_tid (void)
 /* Offset of `stack' member within `struct thread'.
    Used by switch.S, which can't figure it out on its own. */
 uint32_t thread_stack_ofs = offsetof (struct thread, stack);
+
+//test git
