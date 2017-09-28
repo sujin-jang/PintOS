e67de7744cf31aa4f9dc48dd4c2e586c6664abf1
Haney Kang <haney1357@kaist.ac.kr>
2017-09-29 02:49:58 +0900

    syscall_continue

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,6 +3,7 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/init.h"
 
 #include <string.h>
 #include <stdlib.h>
@@ -19,20 +20,77 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf ("system call!\n");
-  hex_dump(f->esp, f->esp, 0X30, true);
-
+  //printf ("system call!\n");
   size_t *syscall_nr = (size_t *)(f->esp);
-
   switch (*syscall_nr) {
-  	case SYS_EXIT: 
+    case SYS_HALT:
+      power_off();
+      break;
+  	case SYS_EXIT:
+      printf("%s: exit(%d)\n","\'process name\'",0);
   		thread_exit();
   		break;
+    case SYS_EXEC:
+      // Not implemented
+      break;
   	case SYS_WAIT:
+      //process_wait();
   		break; 
+    case SYS_CREATE:
+      // Not implemented
+      break;
+    case SYS_REMOVE:
+      // Not implemented
+      break;
+    case SYS_OPEN:
+      // Not implemented
+      break;
+    case SYS_FILESIZE:
+      // Not implemented
+      break;
+    case SYS_READ:
+      // Not implemented
+      break;
   	case SYS_WRITE:
   		syscall_write(f);
   		break;
+    case SYS_SEEK:
+      // Not implemented
+      break;
+    case SYS_TELL:
+      // Not implemented
+      break;
+    case SYS_CLOSE:
+      // Not implemented
+      break;
+/* ------------------------------------------------------------------------*/
+/* 
+ * Haney: No need to implement below since we are working on project 2
+ */
+    case SYS_MMAP:
+      // Not implemented yet
+      break;
+    case SYS_MUNMAP:
+      // Not implemented yet
+      break;
+    case SYS_CHDIR:
+      // Not implemented yet
+      break;
+    case SYS_MKDIR:
+      // Not implemented yet
+      break;
+    case SYS_READDIR: 
+      // Not implemented yet
+      break;
+    case SYS_ISDIR:
+      // Not implemented yet
+      break;
+    case SYS_INUMBER:
+      // Not implemented yet
+      break;
+/* ------------------------------------------------------------------------*/
+    default:
+      break;
   }
 }
 
