059c1004fa3af5efd6e48e8a1b80d8ee2e76e02f
sujin.jang <jsujin9603@gmail.com>
2017-12-12 19:14:30 +0900

    buffer cache ...

--- src/filesys/build/userprog/syscall.d
+++ src/filesys/build/userprog/syscall.d
@@ -1,7 +1,8 @@
 userprog/syscall.o: ../../userprog/syscall.c ../../userprog/syscall.h \
   ../../userprog/process.h ../../threads/thread.h ../../lib/debug.h \
   ../../lib/kernel/list.h ../../lib/stdbool.h ../../lib/stddef.h \
-  ../../lib/stdint.h ../../threads/synch.h ../../userprog/pagedir.h \
+  ../../lib/stdint.h ../../filesys/directory.h ../../devices/disk.h \
+  ../../lib/inttypes.h ../../threads/synch.h ../../userprog/pagedir.h \
   ../../lib/stdio.h ../../lib/stdarg.h ../../lib/kernel/stdio.h \
   ../../lib/syscall-nr.h ../../threads/interrupt.h ../../threads/init.h \
   ../../threads/vaddr.h ../../threads/loader.h ../../threads/palloc.h \
